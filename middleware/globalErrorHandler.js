8693be6139e84076393ee32738bd3ea45d69fc44
itzshakeeb <itzshakeeb@gmail.com>
2023-07-27 00:27:01 +0600

    complite globar error handler

---
+++ middleware/globalErrorHandler.js
@@ -0,0 +1,53 @@
+const ApiError = require("../errors/ApiError");
+const ValidationError = require("../errors/ValidationError");
+const castError = require("../errors/castError");
+
+const globalErrorHandler = (error, req, res, next) => {
+
+    let statusCode = 500;
+    let message = "internal server Error"
+    let errorMessages = []
+
+
+
+    if (error instanceof ApiError) {
+        statusCode = 400;
+        message,
+            errorMessages = error.message ? [
+                {
+                    path: req.originalUrl,
+                    message: error.message
+                }
+            ] : []
+    } else if (error?.name === "CastError") {
+        const structureError = castError(error)
+        statusCode = structureError.statusCode
+        message = structureError.message
+        errorMessages = structureError.errorMessages
+    } else if (error.name === 'ValidationError') {
+        const structureError = ValidationError(error)
+        statusCode = structureError.statusCode
+        message = structureError.message
+        errorMessages = structureError.errorMessages
+    } else if (error instanceof Error) {
+        statusCode,
+            message,
+            errorMessages = [
+                {
+                    path: req.originalUrl,
+                    message: error.message
+                }
+            ]
+    }
+
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+        errorMessages,
+    })
+
+}
+
+module.exports = globalErrorHandler
